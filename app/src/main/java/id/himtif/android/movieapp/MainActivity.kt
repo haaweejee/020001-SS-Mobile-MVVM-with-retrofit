0be7ccea47a12d360f55282aca9603245b1af28f
Rizky Faisal Rafi <87520408+RizkyFaisalRafi@users.noreply.github.com>
2021-12-24 11:12:11 +0700

    Menambahkan Refactor Code

--- app/src/main/java/id/himtif/android/movieapp/MainActivity.kt
+++ app/src/main/java/id/himtif/android/movieapp/MainActivity.kt
@@ -2,14 +2,12 @@ package id.himtif.android.movieapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import id.himtif.android.movieapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
-    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
